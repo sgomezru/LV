29445fef975c60c890bc0ea38e512795ba0a7ec3
Sebastian <segomezru@gmail.com>
2022-09-17 19:20:46 +0200

    Tried DBScan with the whole data, outliers can be detected

--- sol.py
+++ sol.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 import csv
 import plotly.graph_objects as go
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
 
 def cart2pol(x, y):
     rho = np.sqrt(x ** 2 + y ** 2)
@@ -70,6 +72,46 @@ def reduce_data(Measurements, method='mean'):
         
     return reduced
 
+def resultsDBScan(X, eps = 500, min_samples = 3):
+    db = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = labels == k
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=14,
+        )
+
+        xy = X_complete[class_member_mask & ~core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor="k",
+            markersize=6,
+        )
+
+    plt.title(f"Estimated number of clusters: {n_clusters_} \nNoise points found: {n_noise_}")
+    plt.show()
+
 if __name__ == '__main__':
     # data_dir = '/'.join(os.path.abspath(os.path.dirname(__file__)).split('/')[:-1]) + '/data'
     # fpath = os.path.join(data_dir, 'out_startplatz_cut.txt')
@@ -77,5 +119,6 @@ if __name__ == '__main__':
     Measurements, MeasurementsByAngle = read_data(fpath)
     X_complete = arr_pol2cart(Measurements)
     X_reduced = arr_pol2cart(reduce_data(MeasurementsByAngle))
-    print(X_complete.shape, X_reduced.shape)
+    print("Complete data, and reduced data shapes:", X_complete.shape, X_reduced.shape)
+    resultsDBScan(X_complete)
 
